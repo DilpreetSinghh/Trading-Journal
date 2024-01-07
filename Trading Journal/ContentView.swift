edf7b178445ee0461f111bf3d3c6cfc05a251daf
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-07 19:49:26 +0530

    Initial Commit

---
+++ Trading Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trading Journal
+//
+//  Created by Dilpreet Singh on 07/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trading Journal/Trading_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_JournalApp.swift
+//  Trading Journal
+//
+//  Created by Dilpreet Singh on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Trading_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
