e7278dbd902876f1eb99feff7fe1b6c4d4cdfd8f
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-24 22:05:40 +0530

    Changing structure ( @State) and adding colours in home page)

--- Trading Journal/CashDerivative/CashDerivative.swift
+++ Trading Journal/CashDerivative/CashDerivative.swift
@@ -8,81 +8,74 @@
 
 import SwiftUI
 
+// Define a data model that conforms to Codable
+struct CashDerivativeData: Codable {
+    var selectedDate: Date
+    var selectedTime: Date
+    var CDSymbol: String
+    var CDB_TradingType: String
+    var CDI_TradingType: String
+    var CDB_BuyPrice: String
+    var CDI_BuyPrice: String
+    var CDB_SellPrice: String
+    var CDI_SellPrice: String
+    var CDB_StopLoss: String
+    var CDI_StopLoss: String
+    var CDB_Qty: String
+    var CDI_Qty: String
+    var CDB_NetProfit: String
+    var CDI_NetProfit: String
+    var notes: String
+}
+
 struct CashDerivative: View {
     
     // MARK: - State Variables
     
-    // Selected date and time
-    @State  var selectedDate = Date()
-    @State  var selectedTime = Date()
-
-    // Symbol and trading type for Before (B) and After (A)
-    @State  var CDSymbol: String = ""
-    @State var CDB_TradingType: String = ""
-    @State  var CDI_TradingType: String = ""
-    
-    // Buy and sell prices for Before (B) and After (A)
-    @State var CDB_BuyPrice: String = ""
-    @State var CDI_BuyPrice: String = ""
-    @State var CDB_SellPrice: String = ""
-    @State var CDI_SellPrice: String = ""
-    
-    // Stop loss for Before (B) and After (A)
-    @State var CDB_StopLoss: String = ""
-    @State var CDI_StopLoss: String = ""
-    
-    // Quantity for Before (B) and After (A)
-    @State var CDB_Qty: String = ""
-    @State var CDI_Qty: String = ""
-    
-    // Net profit/loss for Before (B) and After (A)
-    @State var CDB_NetProfit: String = ""
-    @State var CDI_NetProfit: String = ""
-    
-    // Notes
-    @State var notes: String = ""
-    
-    // Saved message
-    @State var savedMessage: String = ""
+    // New data model instance
+    @State private var cashDerivativeData = CashDerivativeData(
+        selectedDate: Date(),
+        selectedTime: Date(),
+        CDSymbol: "",
+        CDB_TradingType: "",
+        CDI_TradingType: "",
+        CDB_BuyPrice: "",
+        CDI_BuyPrice: "",
+        CDB_SellPrice: "",
+        CDI_SellPrice: "",
+        CDB_StopLoss: "",
+        CDI_StopLoss: "",
+        CDB_Qty: "",
+        CDI_Qty: "",
+        CDB_NetProfit: "",
+        CDI_NetProfit: "",
+        notes: ""
+    )
     
     // Show saved notes
-    @State var showingSavedNotes = false
+    @State private var showingSavedNotes = false
     
-    // MARK: - Formatters
-    
-    // Date formatter for short date style
-    let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
-    // Time formatter for 24-hour format
-    let timeFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm"
-        return formatter
-    }()
+    // Saved message
+    @State private var savedMessage = ""
     
     // MARK: - Body
     
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVStack(spacing: 20) {
+                LazyVStack {
                     
                     // MARK: - Date and Time Section
                     
                     Group {
                         // Date picker for selecting a date
-                        row(view: AnyView(DatePicker("Date", selection: $selectedDate, in: ...Date(), displayedComponents: .date)))
+                        row(view: AnyView(DatePicker("Date", selection: $cashDerivativeData.selectedDate, in: ...Date(), displayedComponents: .date)))
                         
                         // Time picker for selecting a time
                         Text("Time of Trade")
                             .font(.headline)
                             .padding(.vertical, 5)
-                        row(view: AnyView(DatePicker("", selection: $selectedTime, in: ...Date(), displayedComponents: .hourAndMinute)
+                        row(view: AnyView(DatePicker("", selection: $cashDerivativeData.selectedTime, in: ...Date(), displayedComponents: .hourAndMinute)
                             .datePickerStyle(WheelDatePickerStyle())))
                         
                         // Text field for typing the symbol
@@ -90,11 +83,12 @@ struct CashDerivative: View {
                             Text("Symbol")
                                 .foregroundColor(.primary)
                                 .frame(width: 100, alignment: .leading) // Adjust width as needed
-                            TextField("Type symbol of stock here", text: $CDSymbol)
+                            TextField("Type symbol of stock here", text: $cashDerivativeData.CDSymbol)
                         }))
                     }
                     
                     // MARK: - Trading Details Section
+                    
                     Group {
                         // Horizontal stack for displaying "Before" and "After" labels
                         row(view: AnyView(HStack {
@@ -111,53 +105,51 @@ struct CashDerivative: View {
                             Text("Implement").frame(maxWidth: .infinity, alignment: .center)
                                 .background(Color.gray.opacity(0.3)) // Background color
                             Spacer()
-                            
                         }))
                         
                         // Trading type, buy and sell price, stop loss, quantity, and net profit/loss for both Before (B) and After (A)
                         row(view: AnyView(HStack {
                             Text("Type")
-                            TextField("Buy/Sell", text: $CDB_TradingType)
-                            TextField("Buy/Sell", text: $CDI_TradingType)
+                            TextField("Buy/Sell", text: $cashDerivativeData.CDB_TradingType)
+                            TextField("Buy/Sell", text: $cashDerivativeData.CDI_TradingType)
                         }))
                         
                         row(view: AnyView(HStack {
                             Text("Buy  ")
-                            TextField("Buy Price", text: $CDB_BuyPrice)
-                            TextField("Buy Price", text: $CDI_BuyPrice)
+                            TextField("Buy Price", text: $cashDerivativeData.CDB_BuyPrice)
+                            TextField("Buy Price", text: $cashDerivativeData.CDI_BuyPrice)
                         }))
                         
                         row(view: AnyView(HStack {
                             Text("Sell  ")
-                            TextField("Sell Price", text: $CDB_SellPrice)
-                            TextField("Sell Price", text: $CDI_SellPrice)
+                            TextField("Sell Price", text: $cashDerivativeData.CDB_SellPrice)
+                            TextField("Sell Price", text: $cashDerivativeData.CDI_SellPrice)
                         }))
                         
                         row(view: AnyView(HStack {
                             Text("SL   ")
-                            TextField("Stop Loss", text: $CDB_StopLoss)
-                            TextField("Stop Loss", text: $CDI_StopLoss)
+                            TextField("Stop Loss", text: $cashDerivativeData.CDB_StopLoss)
+                            TextField("Stop Loss", text: $cashDerivativeData.CDI_StopLoss)
                         }))
                         
                         row(view: AnyView(HStack {
                             Text("Qty  ")
-                            TextField("Quantity", text: $CDB_Qty)
-                            TextField("Quantity", text: $CDI_Qty)
+                            TextField("Quantity", text: $cashDerivativeData.CDB_Qty)
+                            TextField("Quantity", text: $cashDerivativeData.CDI_Qty)
                         }))
                         
                         row(view: AnyView(HStack {
                             Text("Net  ")
-                            TextField("Profit/ Loss", text: $CDB_NetProfit)
-                            TextField("Profit/ Loss", text: $CDI_NetProfit)
+                            TextField("Profit/ Loss", text: $cashDerivativeData.CDB_NetProfit)
+                            TextField("Profit/ Loss", text: $cashDerivativeData.CDI_NetProfit)
                         }))
                     }
-                    
                     
                     // Text editor for notes
                     VStack(alignment: .leading) {
                         Text("Notes:")
                             .font(.headline)
-                        TextEditor(text: $notes)
+                        TextEditor(text: $cashDerivativeData.notes)
                             .background(Color.gray.opacity(0.1))
                             .cornerRadius(8.0)
                             .frame(minHeight: 100)
@@ -192,6 +184,16 @@ struct CashDerivative: View {
                 .padding()
             }
             .navigationTitle("Cash Derivative")
+            NavigationLink("Show Saved Notes", destination: SavedNotesView())
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.green)
+                .cornerRadius(8.0)
+            .sheet(isPresented: $showingSavedNotes) {
+                        SavedNotesView()
+                    }
+
+
         }
     }
     
@@ -212,11 +214,35 @@ struct CashDerivative: View {
     // MARK: - Save Data Function
     
     func saveData() {
-        // Perform the data saving logic here using Swift data storage
-        // For demonstration purposes, let's just display a saved message
-        savedMessage = "Data Saved!"
+        // Save the data model to UserDefaults
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(cashDerivativeData) {
+            UserDefaults.standard.set(encodedData, forKey: "cashDerivativeData")
+            savedMessage = "Data Saved!"
+        } else {
+            savedMessage = "Failed to save data."
+        }
     }
+    
+    private func loadSavedNotes() {
+        do {
+            if let encodedData = UserDefaults.standard.data(forKey: "cashDerivativeData") {
+                let decoder = JSONDecoder()
+                let cashDerivativeData = try decoder.decode(CashDerivativeData.self, from: encodedData)
+                
+            }
+        } catch {
+            print("Error loading saved notes: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    
+    
 }
+
+
+
 
 
 #Preview {
