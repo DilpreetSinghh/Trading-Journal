e0791119680b261474f772e321b497a3b7c89571
Dilpreet Singh <114622686+DilpreetSinghh@users.noreply.github.com>
2024-01-27 12:14:21 +0530

    Update Future.swift for photo picker

--- Trading Journal/Future/Future.swift
+++ Trading Journal/Future/Future.swift
@@ -54,6 +54,8 @@ struct FutureDerivative: View {
     
     @State private var showingFDSavedNotes = false
     @State private var fdsavedMessage = ""
+    @State private var isShowingImagePicker = false
+    @State private var selectedImage: UIImage?
     
     // MARK: - Body
     
@@ -129,6 +131,21 @@ struct FutureDerivative: View {
                             .background(Color.gray.opacity(0.1))
                             .cornerRadius(8.0)
                             .frame(minHeight: 100)
+                        
+                        Button("Upload Photo") {
+                            self.isShowingImagePicker = true
+                        }
+                        .sheet(isPresented: $isShowingImagePicker) {
+                            ImagePicker(selectedImage: $selectedImage)
+                        }
+                        
+                        // Display the selected image if available
+                        if let selectedImage = selectedImage {
+                            Image(uiImage: selectedImage)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
                     }
                     
                     // Save and Show Saved Notes Buttons
